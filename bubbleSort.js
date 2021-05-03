778fc5efe59cc54d8e52b1ee2f4ef9744b9bb829
robwatsongtr <rwatso@gmail.com>
2021-05-02 20:32:15 -0700

    bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,1 +1,22 @@
 // bubble sort 
+
+
+function swap(arr, idx1, idx2) {
+  var temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp; 
+}
+
+function bubbleSort(arr) {
+  for( var i = arr.length; i > 0; i-- ) {
+    for( var j = 0; j < i - 1; j++) {
+      // console.log(arr);
+      if( arr[j] > arr[j+1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr; 
+}
+
+console.log( bubbleSort([37,45,29,8,16,12]) );
