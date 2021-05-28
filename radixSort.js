8661264df1acb70d8c351d380906be73d6b97440
robwatsongtr <rwatso@gmail.com>
2021-05-28 08:33:50 -0700

    begin Radix sort.

---
+++ radixSort.js
@@ -0,0 +1,6 @@
+// Radix Sort. Works exclusively with numbers.
+
+// Exploits the fact that information about the size of a number
+// is encoded in the number of digits. More digits means
+// a bigger number.
+
