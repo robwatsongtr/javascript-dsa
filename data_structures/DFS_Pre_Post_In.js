04f32b810c4ffc7dab8ba9d63b7dbd431a73e720
robwatsongtr <rwatso@gmail.com>
2021-06-15 20:31:10 -0700

    more DFS

--- data_structures/DFS_Pre_Post_In.js
+++ data_structures/DFS_Pre_Post_In.js
@@ -2,7 +2,34 @@
 //
 // Depth First Search: PreOrder, PostOrder, InOrder
 //
+/*
 
+PreOrder: Starting at root, visit a node itself, then the left, then right. 
+Node, left, Right.
+Example:
+
+             10
+          6     15
+        3   8     20
+  
+ [10, 6, 3, 8, 15, 20]
+
+---------------------------------------------------------------
+
+PostOrder: All chldren are explored of a node before the node itself, so its
+Left, Right, then Node. 
+
+
+
+
+---------------------------------------------------------------
+
+InOrder: Left, Node, Right
+
+
+
+
+*/
 
 class Node {
   constructor(val) {
@@ -17,7 +44,6 @@ class BinaryTree {
     this.root = null; 
   }
 
-  // INSERT
   insert(val) {
 
     let newNode = new Node(val);
@@ -53,17 +79,59 @@ class BinaryTree {
 
   }
 
-  
+  // push Node value into result array first ("Pre"), then visit left and right 
   DFS_PreOrder() {
-
-
-
-
+    let data = [];
     
+    // recursive helper function 
+    function traverse( node ) {
+      data.push( node.val ); // push the value of the node into data array 
+      if( node.left ) traverse( node.left );
+      if( node.right ) traverse( node.right ); 
+    }
+
+    traverse( this.root ); // the actual function call
+    return data; 
   }
+
+  // Visit Left and Right then Node, THEN put results into array ("Post")
+  DFS_PostOrder() {
+    let data = []
+
+    function traverse( node ) {
+      if( node.left ) traverse( node.left );
+      if( node.right ) traverse ( node.right );
+      data.push( node.val );
+    }
+
+    traverse( this.root ); // the actual function call
+    return data; 
+  }
+
+  // Visit the Left, Node, then Right, put results in to array ("In")
+  DFS_InOrder() {
+    let data = [];
+
+    function traverse( node ) {
+      if( node.left ) traverse( node.left );
+      data.push( node.val );
+      if( node.right ) traverse( node.right );
+    }
+
+    traverse( this.root );
+    return data; 
+  }
+
 }
 
 var myTree = new BinaryTree()
+
+/*
+           10
+      5        13
+    2   7    11   16
+
+*/
 
 myTree.insert(10);
 myTree.insert(5);
@@ -73,4 +141,8 @@ myTree.insert(2);
 myTree.insert(16);
 myTree.insert(7);
 
-console.log( myTree.DFS_PreOrder() ); // 
+
+
+console.log( myTree.DFS_PreOrder() ); // [10, 5, 2, 7, 13, 11, 16]
+console.log( myTree.DFS_PostOrder() ); // [2, 7, 5, 11, 16, 13, 10 ]
+console.log( myTree.DFS_InOrder() ); // [2, 5, 7, 10, 11, 13, 16]
