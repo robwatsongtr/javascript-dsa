733d9d981d7a9a8dd0df01716ac29f1040e3c14b
robwatsongtr <rwatso@gmail.com>
2021-06-17 21:18:17 -0700

    begin graph section

---
+++ data_structures/graph.js
@@ -0,0 +1,10 @@
+/*
+GRAPHS
+
+_Undirected graph_: two way connection or no specified connection of the edge
+between two verticies. _Directed graph_: one way connection between two verticies.
+
+_Unweighted graph_: no value or magnitude associated with each edge, 
+_Weighted graph_ has a value associated with an edge. 
+
+*/
