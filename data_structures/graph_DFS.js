abe9306225ea91b8fdcaaab09eef39f3449dc78f
robwatsongtr <rwatso@gmail.com>
2021-06-25 14:58:28 -0700

    working on graph DFS

--- data_structures/graph_DFS.js
+++ data_structures/graph_DFS.js
@@ -25,3 +25,69 @@ DFS_RECUR(Vertex)
 
 */
 
+class Graph {
+  constructor() {
+    this.adjacencyList = {}; 
+  }
+
+  addVertex( vertex ) {
+    if( !this.adjacencyList[vertex] ) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge( vertex1, vertex2 ) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge( vertex1, vertex2 ) { 
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+    )
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+    )
+  }
+
+  removeVertex( vertex ) {
+    while( this.adjacencyList[vertex].length ) {
+        const adjacentVertex = this.adjacencyList[vertex].pop();
+        this.removeEdge( vertex, adjacentVertex );
+    }
+    delete this.adjacencyList[vertex]; // delete the vertex itself
+  }
+
+}
+/*
+Example graph:
+
+          A
+       /     \
+      B       C
+      |       |
+      D ----- E
+      \       /
+       \     /
+          F
+*/        
+
+// set up example graph 
+let g = new Graph();
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+console.log(g.adjacencyList);
