9186575072c95d914c43806716b65c4bb0d3ca1a
robwatsongtr <rwatso@gmail.com>
2021-07-29 22:36:23 -0700

    begin doubly linked lists

---
+++ data_structures/doublyLinkedList.js
@@ -0,0 +1,20 @@
+// Doubly Linked List
+
+class Node  {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  
+
+}
