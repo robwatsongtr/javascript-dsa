16586333799a4c7eee1f63a4c0bbedfa76d8f1bc
robwatsongtr <rwatso@gmail.com>
2021-08-07 18:06:55 -0700

    working on binary heap

--- data_structures/binaryHeap.js
+++ data_structures/binaryHeap.js
@@ -1,28 +1,28 @@
 /*
 
-HEAPS:
+BINARY HEAPS:
 
-Heaps are trees. 
+Binary Heaps are trees. 
 
 All heaps are as compact as possible: All the children of each node
 are as full as they can be and left children are filled out first.
 
 --MaxBinaryHeap:
-Each parent node has at most two child nodes. 
-The value of each parent node is a always greater than its child nodes. 
-There are no guarantees between sibling nodes. 
-No implied ordering between siblings. 
+*Each parent node has at most two child nodes. 
+***The value of each PARENT node is a always GREATER than its child nodes. 
+*There are no guarantees between sibling nodes. 
+*No implied ordering between siblings. 
 
 
 --MinBinaryHeap:
-Each parent node has at most two child nodes. 
-The value of each child node is always greater than its parent nodes. 
-There are no guarantees between sibling nodes. 
-No implied ordering between siblings. 
+*Each parent node has at most two child nodes. 
+***The value of each CHILD node is always GREATER than its parent nodes. 
+*There are no guarantees between sibling nodes. 
+*No implied ordering between siblings. 
 
 How are heaps represented? Often flattened in an array, with the 
 root being the first sub, then BFS across the first level, 
-BFS across 2nd level, etc:
+BFS across 2nd level, etc. 
 
 Binary Heap is modeled in an array, essentally. 
 
@@ -38,4 +38,11 @@ For any child node at index n:
 
 */
 
+class MaxBinaryHeap {
+  constructor(value) {
+    this.values = []
+  }
 
+  
+
+}
