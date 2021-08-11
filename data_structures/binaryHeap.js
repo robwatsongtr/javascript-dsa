e274275940723a816634f342e4e11575c4b8d5fc
robwatsongtr <rwatso@gmail.com>
2021-08-10 21:23:09 -0700

    finished insert, added more comments

--- data_structures/binaryHeap.js
+++ data_structures/binaryHeap.js
@@ -35,39 +35,82 @@ For any index of an array n:
 For any child node at index n:
   Its parent is at index ( ( n-1 ) / 2 )floor 
    
+Time Complexity of Binary Heap:
+Insertion: O(log n)
+Removal: O(log n) -> unlike BST, which can be as bad as O(n) 
+Search: O(n) ->  because no implied order along siblings 
 
 */
 
 class MaxBinaryHeap {
   constructor(value) {
-    this.values = []
+    this.array = []
   }
 
-  bubbleUp() {
-    let index = this.values.length - 1;
-    const element = this.values[idx];
 
-    // while conditional necessary so that we don't go to negative indicies;
-    while( index > 0 ) {
-      let parentIdx = Math.floor( (index-1) / 2);
-      let parent = this.values[parentIdx]; 
+  // Insert an element, will result in a proper binary heap and the element
+  // finding its right place 
+  insert(element) {
+    this.array.push(element); // push to end of array
+    this.bubbleUp(); // finds the right place for the value. 
+  }
+  
+  // helper method for insert: 
+  bubbleUp() {
+    let idx = this.array.length - 1; // find index of the end of the array
+    const element = this.array[idx]; // set element to that index 
+
+    // while condition necessary so that we don't go to negative indicies
+    while( idx > 0 ) {
+      let parentIdx = Math.floor( (idx-1) / 2); // calculation to find parent
+      let parent = this.array[parentIdx]; // the current parent in the array 
 
       if( element <= parent ) break; // the vaule has found its final position 
 
       // otherwise, swap
-      this.values[parentIdx] = element; 
-      this.values[idx] = parent; 
+      this.array[parentIdx] = element; // swap element to parent 
+      this.array[idx] = parent; // swap parent to element 
 
-      // and update index to be parent
+      // and then update index to be parent
       idx = parentIdx;
-  
     }
 
   }
 
-  insert(element) {
-    this.values.push(element); // push to end of array
-    this.bubbleUp(); // finds the right place for the value. 
+  // extractMAX: 
+
+  // - 'Remove' the root node, swap it with the smallest (last) element 
+
+  // - Then do the process of 'sinking down' that element down to its proper 
+  //   place by swapping with next smaller children. 
+
+  // - You now have the heap configured to be correct again with the 
+  //   next largest element as the root 
+  extractMax() {
+
   }
 
 }
+
+let heap = new MaxBinaryHeap()
+
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+
+console.log(heap);
+
+/*
+Result heap:
+
+[ 55, 39, 41, 18, 27, 12, 33 ]
+
+          55
+      39      41
+    18  27  12  33
+
+*/
