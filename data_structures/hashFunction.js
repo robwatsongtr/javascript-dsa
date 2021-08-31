231b849f1aabdce5c187585fbe6e1bdfd88e1f2a
robwatsongtr <rwatso@gmail.com>
2021-08-30 22:19:54 -0700

    hash tables notes on handling collsions

--- data_structures/hashFunction.js
+++ data_structures/hashFunction.js
@@ -21,4 +21,20 @@ A main way of preventing collisions is to use prime numbers, in that if the size
 of your table / array is a prime number collisions are greatly reduced, by orders
 of magnitude.
 
+--Handling collisions--:
+
+Separate chaining:
+
+In this method, a collision causes a nested datastrucutre to be created, so if two 
+items are to be stored at index 4 of an array,the first item is stored there,
+then the second item is also stored there as an array at that array index, and to 
+retrieve the get func would loop at that index to find the correct value.
+
+Linear probing:
+
+When there is a collision we search through the array to find the next empty.
+Downside is that you can fill up slots in the table quickly, then you have to 
+figure out how to deal with that situation. 
+
 */
+
