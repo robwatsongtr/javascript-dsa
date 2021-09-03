6acb9fd11a0e2ac2b5492f193205ada9e8864867
robwatsongtr <rwatso@gmail.com>
2021-09-03 14:15:53 -0700

    notes on dijkstra

---
+++ data_structures/dijkstrasAlgo.js
@@ -0,0 +1,12 @@
+// Dijkstra's Algorithm!
+
+/*
+
+One of the most famous and widely used algos in the world. Many
+big proprietray algos are built on top of Dijkstras algo.
+
+What it does is find the shortest path between two verticies 
+in a graph. Tremendous implications and use cases for this.
+
+
+*/
