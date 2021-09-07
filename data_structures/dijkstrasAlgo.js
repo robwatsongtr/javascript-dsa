940bed14fa8149c2d326f0907b15ff568e3a0909
robwatsongtr <rwatso@gmail.com>
2021-09-06 23:47:53 -0700

    added weighted graph

--- data_structures/dijkstrasAlgo.js
+++ data_structures/dijkstrasAlgo.js
@@ -10,3 +10,27 @@ in a graph. Tremendous implications and use cases for this.
 
 
 */
+
+
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+
+  // addVertex
+  // if no duplilcate, make an entry in the adjacently list object
+  // that is { vertex: [] }, name is vertex name, value is empty array.
+  addVertex( vertex ) {
+    if( !this.adjacencyList[vertex] ) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  // instead of storing just a string in the adj list, we will store a
+  // nested object with both the vertex name and its weight
+  addEdge( vertex1, vertex2, weight ) {
+    this.adjacencyList[vertex1].push( { node:vertex2, weight } );
+    this.adjacencyList[vertex2].push( { node:vertex1, weight } );
+  }
+
+}
