c23beeb31d8ae3cdc6a0db942500d0e304629558
robwatsongtr <rwatso@gmail.com>
2021-09-07 15:25:25 -0700

    tested add edge and add vertex for weighted graph

--- data_structures/dijkstrasAlgo.js
+++ data_structures/dijkstrasAlgo.js
@@ -29,8 +29,45 @@ class WeightedGraph {
   // instead of storing just a string in the adj list, we will store a
   // nested object with both the vertex name and its weight
   addEdge( vertex1, vertex2, weight ) {
-    this.adjacencyList[vertex1].push( { node:vertex2, weight } );
-    this.adjacencyList[vertex2].push( { node:vertex1, weight } );
+    this.adjacencyList[vertex1].push( { node: vertex2, weight } );
+    this.adjacencyList[vertex2].push( { node: vertex1, weight } );
   }
 
 }
+
+let wgraph = new WeightedGraph();
+
+wgraph.addVertex("A");
+wgraph.addVertex("B");
+wgraph.addVertex("C");
+
+console.log( wgraph.adjacencyList );
+
+console.log( wgraph.addEdge("A", "B", 9) ); 
+console.log( wgraph.addEdge("A", "C", 5) ); 
+console.log( wgraph.addEdge("B", "C", 7) ); 
+
+console.log( wgraph.adjacencyList ); 
+
+/*
+
+{
+  A: [ { node: 'B', weight: 9 }, { node: 'C', weight: 5 } ],
+  B: [ { node: 'A', weight: 9 }, { node: 'C', weight: 7 } ],
+  C: [ { node: 'A', weight: 5 }, { node: 'B', weight: 7 } ]
+}
+
+*/
+
+/*
+            A -9-- B
+            |     /
+            |    /
+            5   7
+            |  /
+            | /
+            C
+*/
+
+
+
