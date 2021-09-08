10c53a280dfa9dc91ccdc58e57eb8068c56d73c2
robwatsongtr <rwatso@gmail.com>
2021-09-07 18:25:16 -0700

    dijkstra notes

--- data_structures/dijkstrasAlgo.js
+++ data_structures/dijkstrasAlgo.js
@@ -10,8 +10,39 @@ big proprietray algos are built on top of Dijkstras algo.
 What it does is find the shortest path between two verticies 
 in a graph. Tremendous implications and use cases for this.
 
+The algorithm:
+
+1. Every time we look to visit a new node, we pick the with the
+   smallest known distance to visit fist 
+
+2. Once we've moved to that node we look at each of its neighbors.
+
+3. For each neighboring node we calculate the distance by summing
+   the total edges that lead to the node we're checking 
+   _ from the starting node _
+
+4. If the new total distance to a node is lessn than the previous 
+  total, we store the new shorter distance for that node. 
 
 */
+
+/*
+Lets take this graph. Find the shortest path from A to E.
+
+          A------4-------- B
+          |                |
+          2                3
+          |                |
+          C --2--- D --3---E
+          \        |       /
+           \       |      /
+            \      1     /
+             4     |    /
+              \    |   1
+               \   |  /
+                \  | /
+                 \ F
+*/               
 
 
 class WeightedGraph {
@@ -19,17 +50,12 @@ class WeightedGraph {
     this.adjacencyList = {}
   }
 
-  // addVertex
-  // if no duplilcate, make an entry in the adjacently list object
-  // that is { vertex: [] }, name is vertex name, value is empty array.
   addVertex( vertex ) {
     if( !this.adjacencyList[vertex] ) {
       this.adjacencyList[vertex] = [];
     }
   }
 
-  // instead of storing just a string in the adj list, we will store a
-  // nested object with both the vertex name and its weight
   addEdge( vertex1, vertex2, weight ) {
     this.adjacencyList[vertex1].push( { node: vertex2, weight } );
     this.adjacencyList[vertex2].push( { node: vertex1, weight } );
