6be3055336fd05010c62c1a14eb876ef9dddee00
robwatsongtr <rwatso@gmail.com>
2021-09-07 17:46:06 -0700

    separated weightedGraph.js from dijkstras algo

--- data_structures/weightedGraph.js
+++ data_structures/weightedGraph.js
@@ -1,19 +1,5 @@
 
 
-// Dijkstra's Algorithm!
-
-/*
-
-One of the most famous and widely used algos in the world. Many
-big proprietray algos are built on top of Dijkstras algo.
-
-What it does is find the shortest path between two verticies 
-in a graph. Tremendous implications and use cases for this.
-
-
-*/
-
-
 class WeightedGraph {
   constructor() {
     this.adjacencyList = {}
@@ -36,3 +22,40 @@ class WeightedGraph {
   }
 
 }
+
+let wgraph = new WeightedGraph();
+
+wgraph.addVertex("A");
+wgraph.addVertex("B");
+wgraph.addVertex("C");
+
+console.log( wgraph.adjacencyList );
+
+console.log( wgraph.addEdge("A", "B", 9) ); 
+console.log( wgraph.addEdge("A", "C", 5) ); 
+console.log( wgraph.addEdge("B", "C", 7) ); 
+
+console.log( wgraph.adjacencyList ); 
+
+/*
+
+{
+  A: [ { node: 'B', weight: 9 }, { node: 'C', weight: 5 } ],
+  B: [ { node: 'A', weight: 9 }, { node: 'C', weight: 7 } ],
+  C: [ { node: 'A', weight: 5 }, { node: 'B', weight: 7 } ]
+}
+
+*/
+
+/*
+            A -9-- B
+            |     /
+            |    /
+            5   7
+            |  /
+            | /
+            C
+*/
+
+
+
