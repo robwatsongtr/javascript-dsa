eb2d9d39a6e7649e203b9a01fb13e31bb8767129
robwatsongtr <rwatso@gmail.com>
2021-09-08 15:22:26 -0700

    added simple priority queue to dijkstras algo

--- data_structures/dijkstrasAlgo.js
+++ data_structures/dijkstrasAlgo.js
@@ -62,3 +62,41 @@ class WeightedGraph {
   }
 
 }
+
+
+// simple priority queue that uses a sorting algo, not a heap
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue( val, priority ) {
+    this.values.push( { val, priority } );
+    this.sort();
+  };
+
+  dequeue() {
+    return this.values.shift();
+  };
+
+  sort() {
+    this.values.sort( (a, b) => a.priority - b.priority );
+  };
+
+}
+
+var q = new PriorityQueue();
+
+q.enqueue('B', 3)
+q.enqueue('C', 5)
+q.enqueue('D', 2)
+q.enqueue('Q', 20)
+
+console.log( q.values ); // works! 
+
+console.log( q.dequeue() );
+console.log( q.dequeue() );
+console.log( q.dequeue() );
+console.log( q.dequeue() ); // works!
+
+
